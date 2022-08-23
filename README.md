bef24cb5d261b1483984565cca8174af2c8c419e
Eric Thornton <ericthornton43@gmail.com>
2022-08-23 17:15:16 -0600

    Initial release.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# rps
-Rock, Paper Scissors
+# Rock, Paper, Scissors!
+
+_Play the classic game of random selection versus the computer! Can you beat the ~~Math.random()~~ AI opponent?_
+
+**You can play the game [here](https://et-codes.github.io/rps).**
+
+![Rock, Paper, Scissors screenshot](assets/screenshot.png)
+
+This project was written with vanilla HTML, CSS, and JavaScript. It was fun to code, and was completed in a couple of hours.
+
+One challenge I wanted to tackle was to make sure the player had positive feedback when making a selection. The first step was to make the icon grow in size when hovering with the mouse. The problem there was that it's not uncommon that the player makes the same selection as the last round, and the computer does too. In that case, there is no change on the screen except for the score, if the result is not a tie, and that can be easy to miss. To rectify that, I made the display go blank with every update, and then introduced a small delay before updating it again. That way the game results "flash" with every player selection.
+
+**Author:** Eric Thornton
--- script.js
+++ script.js
@@ -1,9 +1,80 @@
+// Initialize variables
 const divScore = document.getElementById('score');
 const divPlayerChoice = document.getElementById('player-choice');
 const divComputerChoice = document.getElementById('computer-choice');
 const divResult = document.getElementById('result');
+const choices = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice;
+let computerChoice;
+let matchResult;
 
-divScore.textContent = `Player: 0  |  Computer: 0`;
-divPlayerChoice.textContent = `Player chose xxxx`;
-divComputerChoice.textContent = `Computer chose xxxx`;
-divResult.textContent = `Player won!`;
+const handleClick = (event) => {
+  playerChoice = event.target.id;
+  const randomIndex = parseInt(Math.random() * 3);
+  computerChoice = choices[randomIndex];
+  getResult();
+  clearDisplay();
+  setTimeout(updateDisplay, 75);
+}
+
+const getResult = () => {
+  let result = 0;  // -1 computer, 0 tie, 1 player
+  if (playerChoice === 'rock') {
+    if (computerChoice === 'scissors') {
+      result = 1;
+    } else if (computerChoice === 'paper') {
+      result = -1;
+    }
+  } else if (playerChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      result = -1;
+    } else if (computerChoice === 'rock') {
+      result = 1;
+    }
+  } else if (playerChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      result = -1;
+    } else if (computerChoice === 'paper') {
+      result = 1;
+    }
+  }
+  if (result === 1) {
+    matchResult = 'Player won!';
+    playerScore += 1;
+  } else if (result === -1) {
+    matchResult = 'Computer won!';
+    computerScore += 1;
+  } else if (result === 0) {
+    matchResult = 'Tie!';
+  }
+}
+
+const clearDisplay = () => {
+  divScore.textContent = '';
+  divPlayerChoice.textContent = '';
+  divComputerChoice.textContent = '';
+  divResult.textContent = '';
+}
+
+const updateDisplay = () => {
+  divScore.textContent = `Player: ${playerScore}  |  Computer: ${computerScore}`;
+  if (playerChoice) {
+    divPlayerChoice.textContent = `Player chose ${playerChoice}`;
+  }
+  if (computerChoice) {
+    divComputerChoice.textContent = `Computer chose ${computerChoice}`;
+  }
+  if (matchResult) {
+    divResult.textContent = matchResult;
+  }
+}
+
+// Set event listeners
+const buttons = document.getElementsByClassName('rps-button');
+for (let button of buttons) {
+  button.addEventListener('click', handleClick);
+}
+
+updateDisplay();
