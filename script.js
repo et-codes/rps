3c2326fa37fe0a96d62fe56d886257a1005d89f4
Eric Thornton <ericthornton43@gmail.com>
2022-08-23 12:25:17 -0600

    Create layout and styles.

---
+++ script.js
@@ -0,0 +1,9 @@
+const divScore = document.getElementById('score');
+const divPlayerChoice = document.getElementById('player-choice');
+const divComputerChoice = document.getElementById('computer-choice');
+const divResult = document.getElementById('result');
+
+divScore.textContent = `Player: 0  |  Computer: 0`;
+divPlayerChoice.textContent = `Player chose xxxx`;
+divComputerChoice.textContent = `Computer chose xxxx`;
+divResult.textContent = `Player won!`;
